4b2228fd57532e2758e719dec952f8d04b94df4a
bernethe <bernethe@bernethe.local>
2019-10-01 20:52:31 -0600

    Segundo commit

---
+++ assets/script-mx.js
@@ -0,0 +1,5 @@
+'use strict';
+var countDownDate,x,now,distance;
+const init = (eAI) => {
+}
+document.addEventListener('DOMContentLoaded', init, !1);
---
+++ assets/script.js
@@ -0,0 +1,2 @@
+"use strict";var countDownDate,x,now,distance,init=function(n){};document.addEventListener("DOMContentLoaded",init,!1);
+//# sourceMappingURL=script.js.map
